65142311dffaa1705b54354c7991fb3cec251613
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2023-02-21 14:50:40 -0800

    add day 8 component and styles

---
+++ src/components/day-8/day-8.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../frame/Frame.scss";
+import "./day-8.scss";
+import Details from "../details/details";
+
+const Day8 = () => {
+    
+  const renderblubbs = () => {
+    const blubbs = [];
+        for (let i = 1; i <= 10; i++) {
+        blubbs.push( <div className={`blubb-${i}`}></div>);
+        }
+    return blubbs;
+  };
+
+  const rendersparkles = () => {
+    const sparkles = [];
+    for (let i = 1; i <= 10; i++) {
+      sparkles.push(<div className={`sparkle-${i}`}></div>);
+    }
+    return sparkles;
+  };
+
+  return (
+    <div>
+      <div className="frame day-8-frame">        
+        <div className="day-8-center">
+          <div className="ball"></div>
+          {renderblubbs()}
+          {rendersparkles()}
+        </div>
+      </div>
+      <Details
+        title="Day 8"
+        linkURL="https://codepen.io/Khrebtova/pen/WNgwxdp"
+      />
+    </div>
+  );
+};
+
+export default Day8;
