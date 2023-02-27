263724a018854849d0210df691a5c2d7e0deca60
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2023-02-27 14:47:41 -0800

    add day 13 and animation

--- src/components/day-13/day-13.jsx
+++ src/components/day-13/day-13.jsx
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Details from "../details/details";
 import { BsPlusCircleFill } from "react-icons/bs";
+import {
+  AiFillMessage,
+  AiFillPhone,
+  AiFillHeart,
+  AiFillCloseCircle,
+} from "react-icons/ai";
 import "./day-13.scss";
 
 const Day13 = () => {
+  const [profileVisible, setProfileVisible] = useState(false);
+  const [selectedProfileId, setSelectedProfileId] = useState(1);
+
   const profiles = [
-    { id: 1, img: "https://100dayscss.com/codepen/13-1.jpg", alt: "img1" },
-    { id: 2, img: "https://100dayscss.com/codepen/13-2.jpg", alt: "img2" },
-    { id: 3, img: "https://100dayscss.com/codepen/13-3.jpg", alt: "img3" },
-    { id: 4, img: "https://100dayscss.com/codepen/13-4.jpg", alt: "img4" },
+    {
+      id: 1,
+      img: "https://100dayscss.com/codepen/13-1.jpg",
+      alt: "img1",
+      name: "Julia Toth",
+    },
+    {
+      id: 2,
+      img: "https://100dayscss.com/codepen/13-2.jpg",
+      alt: "img2",
+      name: "John Doe",
+    },
+    {
+      id: 3,
+      img: "https://100dayscss.com/codepen/13-3.jpg",
+      alt: "img3",
+      name: "Jane Miller",
+    },
+    {
+      id: 4,
+      img: "https://100dayscss.com/codepen/13-4.jpg",
+      alt: "img4",
+      name: "Jess Brown",
+    },
   ];
 
+  const selectedProfile = profiles.find(
+    (profile) => profile.id === selectedProfileId
+  );
   const renderProfiles = () => {
     return profiles.map((profile) => {
       return (
-        <div className="profile" id={`profile-${profile.id}`} key={profile.id}>
+        <div
+          className="profile"
+          id={`profile-${profile.id}`}
+          key={profile.id}
+          onClick={() => {
+            setProfileVisible(true);
+            console.log({ profileVisible });
+            setSelectedProfileId(profile.id);
+          }}
+        >
           <div className="plus">
             <BsPlusCircleFill />
           </div>
@@ -26,7 +67,45 @@ const Day13 = () => {
   };
   return (
     <div>
-      <div className="frame day-13-frame">{renderProfiles()}</div>
+      <div className="frame day-13-frame">
+        {profileVisible ? (
+          <div className="profile-details">
+            <div
+              className="close-button"
+              onClick={() => {
+                setProfileVisible(false);
+                console.log({ profileVisible });
+              }}
+            >
+              <AiFillCloseCircle />
+            </div>
+            <div className="header">
+              <img
+                src="https://100dayscss.com/codepen/13-1-header.jpg"
+                alt="header"
+              />
+            </div>
+            <div className="details">
+              <div className="name">{selectedProfile.name}</div>
+              <div className="buttons">
+                <div className="button">
+                  <AiFillMessage />
+                </div>
+                <div className="button">
+                  <AiFillPhone />
+                </div>
+                <div className="button">
+                  <AiFillHeart />
+                </div>
+              </div>
+            </div>
+            <div className="profile-picture">
+              <img src={selectedProfile.img} alt="profile" />
+            </div>
+          </div>
+        ) : null}
+        <div className="profiles-container">{renderProfiles()}</div>
+      </div>
       <Details
         title="Day 12"
         linkURL="https://codepen.io/Khrebtova/pen/MWqjmLY?editors=0100"
