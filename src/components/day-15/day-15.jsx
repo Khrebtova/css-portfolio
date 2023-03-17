df807d6917f11034e8acd8ed6a2df00771512ce7
Kseniia McFadden <mcfaddenkseniia@gmail.com>
2023-03-17 11:48:59 -0700

    finish day-15 desing and functionality

--- src/components/day-15/day-15.jsx
+++ src/components/day-15/day-15.jsx
@@ -3,33 +3,96 @@ import Details from "../details/details";
 import "./day-15.scss";
 
 const Day15 = () => {
-const [file, setFile] = useState(null);
-const [stage, setStage] = useState("start");
-
-const handleUpload = (e) => {
-    const fileNameArray = e.target.files[0].name.split('');
-    let fileName;
-    fileNameArray.length > 40 ?   fileName = fileNameArray.slice(0, 40).join('') + "..." : fileName = fileNameArray.join('');
+  const [file, setFile] = useState(null);
+  const [stage, setStage] = useState("start");
+  
+  const handleUpload = (e) => {
+    const fileNameArray = e.target.files[0].name.split("");
+    let fileName =
+      fileNameArray.length > 40
+        ? fileNameArray.slice(0, 40).join("") + "..."
+        : fileNameArray.join("");
     console.log(fileName);
-    setFile(fileName);    
-}
+    setFile(fileName);
+  };
 
+  const handleClickUpload = () => {
+    if (!file) {
+      alert("Please select a file");
+    } else {
+      setStage("uploading");
+      setFile(null);
+      setTimeout(() => {
+        setStage("done");
+      }, 3000);
+    }
+  };
+
+  const handleClickReset = () => {
+    setStage("start");
+    setFile(null);    
+  };
+
+  const renderButton = () => {
+    switch (stage) {
+      case "uploading":
+        return <div className="button">Uploading...</div>;
+      case "done":
+        return (
+          <div className="button" onClick={handleClickReset}>
+            Done!
+          </div>
+        );
+      default:
+        return (
+          <div className="button" onClick={handleClickUpload}>
+            Upload file
+          </div>
+        );
+    }
+  };
+
+  const renderHeaderAnimation = () => {
+    switch (stage) {
+      case "uploading":
+        return <div className="header-line uploading"></div>;
+      case "done":
+        return <div className="header-line done"></div>;
+      default:
+        return <div className="header-line"></div>;
+    }
+  };
+  
   return (
     <div>
       <div className="frame day-15-frame">
         <div className="day-15-center">
           <div className="header">Drop file to upload</div>
-          <div className="upload-field">
-            <div className="content"> 
-              <input type="file" className="input" onChange={handleUpload}/>
-              {file && <div className="file-name">{file}</div>}
-              <span className="material-symbols-outlined cloud">
-                cloud_upload
+          {renderHeaderAnimation()}
+          <div
+            className={
+              stage === "start" ? "upload-field" : "upload-field uploading"
+            }
+          >
+            {stage === "done" ? (
+              <span className="material-symbols-outlined check">check</span>
+            ) : null}
+            {stage === "uploading" ? (
+              <span className="material-symbols-outlined loading">
+                autorenew
               </span>
-
+            ) : null}
+            <div className="content">
+              <input type="file" className="input" onChange={handleUpload} />
+              {file && <div className="file-name">{file}</div>}
+              {stage === "start" ? (
+                <span className="material-symbols-outlined cloud">
+                  cloud_upload
+                </span>
+              ) : null}
             </div>
           </div>
-          <div className="button">Upload file</div>
+          {renderButton()}
         </div>
       </div>
       <Details
